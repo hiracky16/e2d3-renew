0427fd0db81009738cc9b16fa77f531e0fcba080
hiracky16 <h.piiice16@gmail.com>
2019-12-15 10:44:41 +0900

    静的ファイルのホスティング

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
+import path from 'path'
+import http from 'http'
+import fs from 'fs'
 
 const app = express();
+
+const graphPath = '.' // e2d3-contrib へのパスを設定する必要あり
+app.use('/graphs', express.static(graphPath))
 
 app.get("/", (req: Request, res: Response) => {
   return res.send("Hello world.");
@@ -8,6 +14,10 @@ app.get("/", (req: Request, res: Response) => {
 
 app.listen(3000, () => {
   console.log("Example app listening on port 3000!");
+	fs.readdir(graphPath, (err, files) => {
+		console.log(files)
+	})
 });
+
 
 export default app;
